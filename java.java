f2172ba42e3a39f31d7340c051bd76af2d0727cb
UMAM AHMAD <a.umam2004@gmail.com>
2025-03-07 16:54:13 +0530

    took another

--- java.java
+++ java.java
@@ -2,37 +2,13 @@ public class int{
 
 public staic void main(String [] args){
 
-
-
-
-
-
-
 System.out.println("Hhiiiii")
 
-
-
-
+System.out.println("Hello world")
 
 
 
 } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
 
