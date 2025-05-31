d611a42ccba18b3b49cee95de05616222a7f32af
Umama <umamahmad2006@gmail.com>
2025-05-31 12:02:20 +0530

    uploaded Q01

---
+++ semester2/assignment3/Q01.java
@@ -0,0 +1,25 @@
+package semester2.assignment3;
+
+import java.util.Scanner;
+
+public class Q01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            System.out.println("Enter your lucky number: ");
+            String str = sc.nextLine();
+            int luckyNumber = Integer.parseInt(str);
+
+            if (luckyNumber<0)
+                throw new NumberFormatException("Negative numbers are not allowed.");
+
+            System.out.println("Your lucky number is: "+luckyNumber);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input: "+e.getMessage());
+        }
+
+
+    }
+}
